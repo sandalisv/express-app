73718296152672cd10239bcb9036225dd891fb92
Sandali Vithanage <sandalisamadara95@gmail.com>
2023-05-02 11:40:28 +0530

    Changed new word to new 2

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World New1!</h1>')
+  res.send('<h1>Hello World New2!</h1>')
 })
 
 const PORT = 8080
 
 app.listen(PORT, () => {
-  console.log(`Server running on port new1 ${PORT}`)
+  console.log(`Server running on port new2 ${PORT}`)
 })
